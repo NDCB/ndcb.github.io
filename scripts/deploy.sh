9835909f8a4e6b15bb478720e005603870ee1d8a
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2018-02-20 13:11:04 -0500

    Set a default locale to deploy at the root

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,11 @@
 echo "Starting deployment"
 echo "Target: master branch"
 
+DEFAULT_LOCALE=`node -p "require('./public/_data.json').website.defaultLocale"` || exit 1
+echo "Default locale set to "$DEFAULT_LOCALE
+
+PROJECT_PATH="./public/"
+
 TEMP_DIRECTORY="/tmp/deploy"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
@@ -9,7 +14,7 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 
 echo "Compiling new static content"
 mkdir $TEMP_DIRECTORY || exit 1
-harp compile public $TEMP_DIRECTORY || exit 1
+harp compile $PROJECT_PATH $TEMP_DIRECTORY || exit 1
 cp .gitignore $TEMP_DIRECTORY || exit 1
 
 echo "Checking out master branch"
@@ -22,12 +27,20 @@ echo "Copying newly generated static content"
 cp -r $TEMP_DIRECTORY/* . || exit 1
 cp $TEMP_DIRECTORY/.gitignore . || exit 1
 
+echo "Copying the default locale at the root path"
+rm -rf $TEMP_DIRECTORY
+mkdir $TEMP_DIRECTORY || exit 1
+cp -r .$DEFAULT_LOCALE/* $TEMP_DIRECTORY || exit 1
+rm -rf .$DEFAULT_LOCALE
+cp -r $TEMP_DIRECTORY/* . || exit 1
+
+echo "Committing deployment"
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
 git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" master > /dev/null 2>&1
 
 echo "Cleaning up temp files"
-rm -Rf $TEMP_DIRECTORY
+rm -rf $TEMP_DIRECTORY
 
 echo "Deployed successfully."
 exit 0
