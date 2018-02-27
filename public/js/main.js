35d4184e68c68b042114522e22b0f43dae30f66c
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2018-02-27 12:46:23 -0500

    Linting

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 /**
  * The attribute for collapsible elements.
  */
-var expanded = "aria-expanded";
+const expanded = 'aria-expanded';
 
-//Modules
+// Modules
 
 /**
  * Handles the click event of modules' toggler icon.
  */
-$('.module i.toggler').on('click', function () {
+$('.module i.toggler').on('click', function() {
     toggleModuleExpansion($(this));
 });
 
@@ -18,25 +18,25 @@ $('.module i.toggler').on('click', function () {
  */
 function toggleModuleExpansion(moduleToggler) {
     moduleToggler.toggleClass('rotate');
-    var moduleContent = moduleToggler.parent('header').next('.content');
-    if (moduleContent.attr(expanded) === "false") {
-        moduleContent.attr(expanded, "true");
+    let moduleContent = moduleToggler.parent('header').next('.content');
+    if (moduleContent.attr(expanded) === 'false') {
+        moduleContent.attr(expanded, 'true');
     } else {
-        moduleContent.attr(expanded, "false");
+        moduleContent.attr(expanded, 'false');
     }
 }
 
-//Primary navigation
+// Primary navigation
 
 /**
  * The toggler icon for the primary navigation.
  */
-var navigationToggler = $('#primary-navigation header i.toggler');
+const navigationToggler = $('#primary-navigation header i.toggler');
 
 /**
  * The content of the primary navigation.
  */
-var navigationContent = navigationToggler.parent().next('ul');
+const navigationContent = navigationToggler.parent().next('ul');
 
 /**
  * Updates the display of the primary navigation's content when the window
@@ -52,23 +52,23 @@ $(window).on('resize', function() {
 $(updateNavigationDisplay());
 
 /**
- * Updates the display of the primary navigation's content based on the width 
+ * Updates the display of the primary navigation's content based on the width
  * of the device.
  */
 function updateNavigationDisplay() {
     if (isDesktopWidth()) {
         navigationContent.show();
-        navigationContent.attr(expanded, "true");
+        navigationContent.attr(expanded, 'true');
     } else {
         navigationContent.hide();
-        navigationContent.attr(expanded, "false");
+        navigationContent.attr(expanded, 'false');
     }
 }
 
 /**
  * Handles the click event of the primary navigation's toggler icon.
  */
-navigationToggler.on('click', function () {
+navigationToggler.on('click', function() {
     toggleNavigationExpansion();
 });
 
@@ -77,17 +77,17 @@ navigationToggler.on('click', function () {
  */
 function toggleNavigationExpansion() {
     toggleElementAnimated(navigationContent);
-    if (navigationContent.attr(expanded) === "false") {
-        navigationContent.attr(expanded, "true");
+    if (navigationContent.attr(expanded) === 'false') {
+        navigationContent.attr(expanded, 'true');
     } else {
-        navigationContent.attr(expanded, "false");
+        navigationContent.attr(expanded, 'false');
     }
 }
 
 /**
  * Handles the click event of the navigation elements.
  */
-$('#primary-navigation ul li i.toggler').on('click', function () {
+$('#primary-navigation ul li i.toggler').on('click', function() {
     toggleNavigationElementExpansion($(this));
 });
 
@@ -98,31 +98,31 @@ $('#primary-navigation ul li i.toggler').on('click', function () {
  */
 function toggleNavigationElementExpansion(navigationElementToggler) {
     navigationElementToggler.toggleClass('rotate');
-    var navigationElementContent = navigationElementToggler.next('ul');
+    let navigationElementContent = navigationElementToggler.next('ul');
     toggleElementAnimated(navigationElementContent);
-    if (navigationElementContent.attr(expanded) === "false") {
-        navigationElementContent.attr(expanded, "true");
+    if (navigationElementContent.attr(expanded) === 'false') {
+        navigationElementContent.attr(expanded, 'true');
     } else {
-        navigationElementContent.attr(expanded, "false");
+        navigationElementContent.attr(expanded, 'false');
     }
 }
 
-//Util
+// Util
 
 /**
- * Toggles a given element with an animation if the device's scrren is wide 
+ * Toggles a given element with an animation if the device's scrren is wide
  * enough.
  * @param {jQuery} element the element to toggle.
  */
 function toggleElementAnimated(element) {
     if (isTabletPortraitWidth()) {
-        if (element.attr(expanded) === "false") {
+        if (element.attr(expanded) === 'false') {
             element.slideDown('fast');
         } else {
             element.slideUp('fast');
         }
     } else {
-        if (element.attr(expanded) === "false") {
+        if (element.attr(expanded) === 'false') {
             element.show();
         } else {
             element.hide();
@@ -131,18 +131,18 @@ function toggleElementAnimated(element) {
 }
 
 /**
- * Returns whether or not the media has the minimum width of the tabvar portrait 
+ * Returns whether or not the media has the minimum width of the tabvar portrait
  * breakpoint.
- * @returns {Boolean} if the media has the minimum width of a tablet.
+ * @return {Boolean} if the media has the minimum width of a tablet.
  */
 function isTabletPortraitWidth() {
     return $(window).width() >= 768;
 }
 
 /**
- * Returns whether or not the media has the minimum width of the desktop 
+ * Returns whether or not the media has the minimum width of the desktop
  * computer breakpoint.
- * @returns {Boolean} if the media has the minimum width of a desktop computer.
+ * @return {Boolean} if the media has the minimum width of a desktop computer.
  */
 function isDesktopWidth() {
     return $(window).width() >= 1200;
