49f6544cf8fae029c25b628a2c271997d866c596
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2018-02-28 17:19:39 -0500

    Better folder structure for Prism grammars

--- public/js/prism-config.js
+++ public/js/prism-config.js
@@ -1,4 +1,4 @@
 /**
  * Configures the path to the grammar components of the Prism autoloader plugin.
  */
-Prism.plugins.autoloader.languages_path = '/prism-grammars/';
+Prism.plugins.autoloader.languages_path = '/js/prism-grammars/';
