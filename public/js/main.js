9e61dde1ebd9d7931198e2c020affae47cec47b6
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2018-02-28 17:20:00 -0500

    Merge main.js and share.js

--- public/js/main.js
+++ public/js/main.js
@@ -147,3 +147,36 @@ function isTabletPortraitWidth() {
 function isDesktopWidth() {
     return $(window).width() >= 1200;
 }
+
+// Share
+
+/**
+ * Summons a pop-up window.
+ * @param {string} url the URL featured in the pop-up.
+ * @param {number} width the width of the pop-up window.
+ * @param {number} height the height of the pop-up window.
+ */
+function windowPopup(url, width, height) {
+    const left = (screen.width / 2) - (width / 2);
+    const top = (screen.height / 2) - (height / 2);
+    window.open(
+      url,
+      '',
+      'menubar=no'
+      +',toolbar=no'
+      +',resizable=yes'
+      +',scrollbars=yes'
+      +',width=' + width
+      + ',height=' + height
+      + ',top=' + top
+      + ',left=' + left
+    );
+  }
+
+  /**
+   * Event handler for click events on social share pop-up anchors.
+   */
+  $('a.social-share-popup').on('click', function(event) {
+    event.preventDefault();
+    windowPopup($(this).attr('href'), 500, 500);
+  });
